6f2eb87df87bf0c18952dde3388f217bf3ca8905
Jannik Kramer <kramer@consider-it.de>
2017-09-25 11:17:29 +0200

    Added raw bootstrap structure

--- paperyard/frontend/src/routes.php
+++ paperyard/frontend/src/routes.php
@@ -5,10 +5,6 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/', function (Request $request, Response $response, array $args) {
+    return $this->view->render($response, 'index.twig', []);
 });

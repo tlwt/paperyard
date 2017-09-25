f56b4deb4d197cc07607c67c25302f50c34b015e
Jannik Kramer <kramer@consider-it.de>
2017-09-25 11:17:29 +0200

    Added raw bootstrap structure

--- paperyard/frontend/src/dependencies.php
+++ paperyard/frontend/src/dependencies.php
@@ -1,19 +1,19 @@
 <?php
 // DIC configuration
 
+// Get container
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
+// Register component on container
+$container['view'] = function ($container) {
+    $settings = $container->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path'], [
+        'cache' => false
+    ]);
 
-// monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-    return $logger;
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
 };

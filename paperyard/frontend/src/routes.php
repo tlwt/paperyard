40eef4e5e48f85c051daf729c8c808f922e32af4
Jannik Kramer <kramer@consider-it.de>
2017-09-26 14:16:26 +0200

    removed custom template, added CoreUI template, fixed issue #1

--- paperyard/frontend/src/routes.php
+++ paperyard/frontend/src/routes.php
@@ -6,6 +6,5 @@ use Slim\Http\Response;
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $indexView = new Paperyard\Views\IndexView();
-    return $this->view->render($response, 'index.twig', $indexView->render());
+    return $this->view->render($response, 'index.twig', []);
 });

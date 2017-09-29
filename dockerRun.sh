5d61c8fde4716126237bf1d4bf29d4faadfdfa63
Till S. Witt <witt@consider-it.de>
2017-09-29 16:10:00 +0200

    dev and production setup
    fixes #26

--- dockerRun.sh
+++ dockerRun.sh
@@ -10,13 +10,29 @@ renamedFiles=$HOME/Archiv/10_System/21_renamed_with_paperyard
 # files which will be sorted
 toSort=$HOME/Archiv/10_System/22_checked_and_to_be_archived
 
-# running the container
-docker run --name ppyrd --rm \
-  -v "$(pwd)/paperyard:/var/www/html/" \
-  -v "$(pwd)/data/database:/data/database" \
-  -v "$unocredFiles:/data/scan" \
-  -v "$unnamedFiles:/data/inbox" \
-  -v "$renamedFiles:/data/outbox" \
-  -v "$toSort:/data/sort" \
-  -p 80:80 \
-  -i -t ppyrd_image
+
+# set to true to use local modified app instead of current published version from github
+localDevelopment=false
+
+if $localDevelopment ; then
+  echo local
+  docker run --name ppyrd --rm \
+      -v "$(pwd)/data/database:/data/database" \
+      -v "$(pwd)/paperyard:/var/www/html/" \
+      -v "$unocredFiles:/data/scan" \
+      -v "$unnamedFiles:/data/inbox" \
+      -v "$renamedFiles:/data/outbox" \
+      -v "$toSort:/data/sort" \
+      -p 80:80 \
+      -i -t ppyrd_image
+else
+  echo from github
+  docker run --name ppyrd --rm \
+      -v "$(pwd)/data/database:/data/database" \
+      -v "$unocredFiles:/data/scan" \
+      -v "$unnamedFiles:/data/inbox" \
+      -v "$renamedFiles:/data/outbox" \
+      -v "$toSort:/data/sort" \
+      -p 80:80 \
+      -i -t ppyrd_image
+fi

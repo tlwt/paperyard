92f12bb50d52cb40726cdb9857f06f31f6c661d8
Till S. Witt <witt@consider-it.de>
2017-10-13 10:14:05 +0200

    Moving stuff to Dev and Deploy

--- paperyard/docAndQuality/coverityInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /
-rm -rf /cov-analysis-linux*
-wget https://scan.coverity.com/download/linux64 --post-data "token=-oTLrr-4-FvxiEFj2SFjTw&project=tlwt%2Fpaperyard" -O coverity_tool.tgz
-gzip -d coverity_tool.tgz
-tar -xvf coverity_tool.tar
-rm coverity_tool.tar
-mv cov-analysis-linux64* cov-analysis-linux

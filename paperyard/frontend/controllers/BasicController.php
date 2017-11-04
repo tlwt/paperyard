d44ebb6b9928c69b9eff82aa78c499a5cc8a860d
Unknown <kramer@consider-it.de>
2017-11-04 15:46:21 +0100

    improved documentation on BasicController

--- paperyard/frontend/controllers/BasicController.php
+++ paperyard/frontend/controllers/BasicController.php
@@ -4,17 +4,33 @@ namespace Paperyard\Controllers;
 
 class BasicController
 {
+    /** @var \Slim\Views\Twig */
     protected $view;
+
+    /** @var \Psr\Log\LoggerInterface */
     protected $logger;
+
+    /** @var \Slim\Flash\Messages */
     protected $flash;
 
+    /** @var array */
     private $plugins;
 
+    /**
+     * return successfully registered plugins
+     *
+     * @return array plugins which exist
+     */
     public function getPlugins()
     {
         return $this->plugins;
     }
 
+    /**
+     * checks if plugin exist and adds it to the plugin list
+     *
+     * @param $name string plugin to search for in plugin directory
+     */
     public function registerPlugin($name)
     {
         if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/frontend/public/static/js/plugins/' . $name . '.js')) {
@@ -24,6 +40,11 @@ class BasicController
         }
     }
 
+    /**
+     * maps current set language to html class
+     *
+     * @return string
+     */
     public function getLanguageFlag() {
         $codes = array(
             "de_DE" => "flag-icon-de",

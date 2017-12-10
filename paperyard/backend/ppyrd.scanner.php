c1b637e7b01d91f06893d369a0ee09b56a50c84a
Till Witt <mail@tillwitt.de>
2017-12-10 17:51:45 +0100

    Now?

--- paperyard/backend/ppyrd.scanner.php
+++ paperyard/backend/ppyrd.scanner.php
@@ -1,60 +1,64 @@
 <?php
-	require_once('dbHandler.php');
-	require_once('ppyrd.base.php');
+  require_once('dbHandler.php');
+  require_once('ppyrd.base.php');
 
 
-	/**
-	 * \brief takes PDFs and runs OCRmyPDF on them
-	 */
+  /**
+   * \brief takes PDFs and runs OCRmyPDF on them
+   */
 
-	class pdfScanner extends ppyrd
-	{
-		/**
-		 * \brief constructor taking care of setup
-		 */
-		function __construct($pdf,$db)
-		{
-				$this->pdf = $pdf;
-				$this->db = $db;
-				$this->tesseractCommand = $this->db->getConfigValue('tesseractCommand');
-		}
+  class pdfScanner extends ppyrd
+  {
+    /**
+     * \brief constructor taking care of setup
+     */
+    function __construct($pdf,$db)
+    {
+        $this->pdf = $pdf;
+        $this->db = $db;
+        $this->tesseractCommand = $this->db->getConfigValue('tesseractCommand');
+    }
 
-		/**
-		 * \brief function executing main logic
-		 */
-		function run()
-		{
-			$this->output("executing on " . $this->pdf);
-			// ensuring that we only have one OcrMyPDF running process running
-			// due to cron usage and large PDFs this could be an issue
-			$fp = fopen('/tmp/ppyrdOcrMyPdf.txt', 'w+');
+    /**
+     * \brief function executing main logic
+     */
+    function run()
+    {
+      $this->output("executing on " . $this->pdf);
+      // ensuring that we only have one OcrMyPDF running process running
+      // due to cron usage and large PDFs this could be an issue
+      $fp = fopen('/tmp/ppyrdOcrMyPdf.txt', 'w+');
 
-			// checking if lock has been set properly
-			if(flock($fp, LOCK_EX))
-			{
-					// running OCRmyPDF
-					exec($this->tesseractCommand . " '" . $this->pdf . "' '/data/inbox/" . $this->pdf . "'");
-					if (file_exists("/data/inbox/" . $this->pdf))
-					{
-							$this->output("found ok OCR - moving input to archive");
-							exec("mv --backup=numbered '" . $this->pdf . "' '/data/scan/archive/" . $this->pdf . "'");
-					} else
-					{
-						$this->output("did not find ok OCR - moving input to error");
-						exec("mv --backup=numbered '" . $this->pdf . "' '/data/scan/error/" . $this->pdf . "'");
-					}
+      // checking if lock has been set properly
+      if(flock($fp, LOCK_EX))
+      {
+          // running OCRmyPDF
+          exec($this->tesseractCommand . " '" . $this->pdf . "' '/data/inbox/" . $this->pdf . "'");
+          if (file_exists("/data/inbox/" . $this->pdf))
+          {
+              // fixing user permissions since OCR is run as www-data
+              $this->output("fixing permissions");
+              $this->output( "user_id=$(stat -c '%u:%g' " . $this->pdf . '); chown $user_id /data/inbox/' . $this->pdf);
+              exec ("user_id=$(stat -c '%u:%g' " . $this->pdf . '); chown $user_id /data/inbox/' . $this->pdf);
+              $this->output("found ok OCR - moving input to archive");
+              exec("mv --backup=numbered '" . $this->pdf . "' '/data/scan/archive/" . $this->pdf . "'");
+          } else
+          {
+            $this->output("did not find ok OCR - moving input to error");
+            exec("mv --backup=numbered '" . $this->pdf . "' '/data/scan/error/" . $this->pdf . "'");
+          }
 
-			} else
-			{
-				// this will be echoed in case OCRmyPDF is still running
-				$this->output("OcrMyPdf still running - cannot interfere with it ... if this persists too long check /tmp/ppyrOcrMyPdf.txt and delete");
-			}
+      } else
+      {
+        // this will be echoed in case OCRmyPDF is still running
+        $this->output("OcrMyPdf still running - cannot interfere with it ... if this persists too long check /tmp/ppyrOcrMyPdf.txt and delete");
+      }
 
-			// closing lock again - so other instances can be started.
-			fclose($fp);
+      // closing lock again - so other instances can be started.
+      fclose($fp);
 
-		}
-	}
+    }
+  }
 
 // main program
 
@@ -66,8 +70,6 @@ $ppyrd = new ppyrd($db);
 
 
 // looping main directory and calling the pdf parser
-$ppyrd->output("starting paperyard");
-
 
 /**
  * checking if called via command line or webserver
@@ -76,14 +78,13 @@ $ppyrd->output("starting paperyard");
 
 
 // checking if any new PDFs need to be OCRed
-$ppyrd->output( "calling OcrMyPDF ... ");
 chdir("/data/scan");
 
 //loop all pdfs
 $pdfs = glob("*.pdf");
 foreach($pdfs as $pdf){
     $pdf=new pdfScanner($pdf, $db);
-		$pdf->run();
+    $pdf->run();
 }
 
 $db->close();
--- paperyard/backend/ppyrd.sorter.php
+++ paperyard/backend/ppyrd.sorter.php
@@ -1,141 +1,141 @@
 <?php
-	require_once('dbHandler.php');
-	require_once('ppyrd.base.php');
+  require_once('dbHandler.php');
+  require_once('ppyrd.base.php');
 
 
-	/**
-	 * \brief Sorts thru PDF documents and puts them into corresponding folders etc.
-	 * @param none
-	 * @return none
-	 */
-	class pdfSorter extends ppyrd {
+  /**
+   * \brief Sorts thru PDF documents and puts them into corresponding folders etc.
+   * @param none
+   * @return none
+   */
+  class pdfSorter extends ppyrd {
 
-		/**
-		 * \brief constructor
-		 * @param string $pdf file name to be processed
-		 * @param string $db database handler
-		 * @return none
-		 */
-		public function __construct($pdf, $db)
-		{
-				$this->pdf = $pdf;
+    /**
+     * \brief constructor
+     * @param string $pdf file name to be processed
+     * @param string $db database handler
+     * @return none
+     */
+    public function __construct($pdf, $db)
+    {
+        $this->pdf = $pdf;
 
-				// creating db handler to talk to DB
-				$this->db=$db;
-		}
+        // creating db handler to talk to DB
+        $this->db=$db;
+    }
 
-		/**
-		 * \brief function gets from file name the information what the date, company and subject is.
-		 */
-		function splitUpFilename()
-		{
-			$this->output("working on: " . $this->pdf);
+    /**
+     * \brief function gets from file name the information what the date, company and subject is.
+     */
+    function splitUpFilename()
+    {
+      $this->output("working on: " . $this->pdf);
 
-			// getting the file name template - needed to see what part (date, company, subject) is at which part of the string
-			$newFilenameStructure=$this->db->getConfigValue('newFilenameStructure');
+      // getting the file name template - needed to see what part (date, company, subject) is at which part of the string
+      $newFilenameStructure=$this->db->getConfigValue('newFilenameStructure');
 
-			// getting things via regex
-			// removing closing parts of the brackets as they are not needed in this case
-			$unwanted = array(')',']');
-			$separators = "/ - | \(| \[| \-\- /";
-			$templateName = str_replace($unwanted, '', $newFilenameStructure);
+      // getting things via regex
+      // removing closing parts of the brackets as they are not needed in this case
+      $unwanted = array(')',']');
+      $separators = "/ - | \(| \[| \-\- /";
+      $templateName = str_replace($unwanted, '', $newFilenameStructure);
 
-			// splitting up template name into parts
-			$templateParts = array_flip(preg_split($separators, $templateName));
+      // splitting up template name into parts
+      $templateParts = array_flip(preg_split($separators, $templateName));
 
-			// now doing the same stuff with the actual file
-			$tmpName = str_replace($unwanted, '', $this->pdf);
-			$filenameParts = preg_split($separators, $tmpName);
-
-
-
-			// separating the file name into its parts
-			$parts = explode(" - ", $this->pdf);
-
-			// date is 1st
-			$this->date = $filenameParts[$templateParts['ddatum']];
-			$this->year = date('Y',strtotime($this->date));
-			$this->month = date('m',strtotime($this->date));
-			$this->day = date('d',strtotime($this->date));
-
-			// company etc.
-			$this->company = $filenameParts[$templateParts['ffirma']];
-			$this->recipient = $filenameParts[$templateParts['wwer']];
-			$this->subject = $filenameParts[$templateParts['bbetreff']];
-			$this->amount = $filenameParts[$templateParts['bbetrag']];
+      // now doing the same stuff with the actual file
+      $tmpName = str_replace($unwanted, '', $this->pdf);
+      $filenameParts = preg_split($separators, $tmpName);
 
 
 
-			// getting all tags @todo - needs to be least greedy ...
-			preg_match_all('/\[(\d|\w)*\]/',$this->pdf,$tags);
-			$this->tags = implode($tags[0]);
+      // separating the file name into its parts
+      $parts = explode(" - ", $this->pdf);
 
-			//
-			$this->output( "date: " . $this->date);
-			$this->output( "year: " . $this->year);
-			$this->output( "month: " . $this->month);
-			$this->output( "day: " . $this->day);
+      // date is 1st
+      $this->date = $filenameParts[$templateParts['ddatum']];
+      $this->year = date('Y',strtotime($this->date));
+      $this->month = date('m',strtotime($this->date));
+      $this->day = date('d',strtotime($this->date));
 
-			$this->output( "company: " . $this->company);
-			$this->output( "recipient: " . $this->recipient);
-			$this->output( "subject: " . $this->subject);
-			$this->output( "amount: " . $this->amount);
-			$this->output( "tags: " . $this->tags);
+      // company etc.
+      $this->company = $filenameParts[$templateParts['ffirma']];
+      $this->recipient = $filenameParts[$templateParts['wwer']];
+      $this->subject = $filenameParts[$templateParts['bbetreff']];
+      $this->amount = $filenameParts[$templateParts['bbetrag']];
 
-		}
 
-		/**
-		 * \brief checks rules
-		 */
-		function checkRules()
-		{
-			$rules = $this->db->getActiveArchiveRules();
-			while ($row = $rules->fetchArray()) {
-				// we have  a rule match if the company found matches the specified string
-				// * is the wild card like in file names
-				$match = fnmatch($row['company'], $this->company)
-								&& fnmatch($row['subject'], $this->subject)
-								&& fnmatch($row['recipient'], $this->recipient)
-								&& fnmatch($row['tags'], $this->tags);
 
-				// if everything matched - go ahead
-				if ($match) {
-						// processing the folder to which document shall be moved
-						$toFolder = $row['toFolder'];
+      // getting all tags @todo - needs to be least greedy ...
+      preg_match_all('/\[(\d|\w)*\]/',$this->pdf,$tags);
+      $this->tags = implode($tags[0]);
 
-						// in case the [year] variable has been used etc.
-						$toFolder = str_replace('[year]', $this->year, $toFolder);
-						$toFolder = str_replace('[month]', $this->month, $toFolder);
-						$toFolder = str_replace('[day]', $this->day, $toFolder);
-						$toFolder = str_replace('[recipient]', $this->recipient, $toFolder);
+      //
+      $this->output( "date: " . $this->date);
+      $this->output( "year: " . $this->year);
+      $this->output( "month: " . $this->month);
+      $this->output( "day: " . $this->day);
 
-						// adding a trailing slash in case none existed
-						$toFolder = rtrim($toFolder, '/') . '/';
+      $this->output( "company: " . $this->company);
+      $this->output( "recipient: " . $this->recipient);
+      $this->output( "subject: " . $this->subject);
+      $this->output( "amount: " . $this->amount);
+      $this->output( "tags: " . $this->tags);
 
-						// create folders in case required
-						exec("mkdir -p $toFolder");
+    }
 
-						// move the file to destination folder
-						exec('mv --backup=numbered "' . $this->pdf . '" "' . $toFolder . $this->pdf . '"');
+    /**
+     * \brief checks rules
+     */
+    function checkRules()
+    {
+      $rules = $this->db->getActiveArchiveRules();
+      while ($row = $rules->fetchArray()) {
+        // we have  a rule match if the company found matches the specified string
+        // * is the wild card like in file names
+        $match = fnmatch($row['company'], $this->company)
+                && fnmatch($row['subject'], $this->subject)
+                && fnmatch($row['recipient'], $this->recipient)
+                && fnmatch($row['tags'], $this->tags);
 
-						$this->db->writeLog($this->pdf, $this->pdf, "", "Moved file to: " . $toFolder);
-				}
-			}
-		}
+        // if everything matched - go ahead
+        if ($match) {
+            // processing the folder to which document shall be moved
+            $toFolder = $row['toFolder'];
 
-		/**
-		 * \brief runs the main process
-		 */
-		function run()
-		{
+            // in case the [year] variable has been used etc.
+            $toFolder = str_replace('[year]', $this->year, $toFolder);
+            $toFolder = str_replace('[month]', $this->month, $toFolder);
+            $toFolder = str_replace('[day]', $this->day, $toFolder);
+            $toFolder = str_replace('[recipient]', $this->recipient, $toFolder);
 
-			// process the file name first
-			$this->splitUpFilename();
+            // adding a trailing slash in case none existed
+            $toFolder = rtrim($toFolder, '/') . '/';
 
-			// then see if there is any rule to process
-			$this->checkRules();
-		}
-	}
+            // create folders in case required
+            exec("mkdir -p $toFolder");
+
+            // move the file to destination folder
+            exec('mv --backup=numbered "' . $this->pdf . '" "' . $toFolder . $this->pdf . '"');
+
+            $this->db->writeLog($this->pdf, $this->pdf, "", "Moved file to: " . $toFolder);
+        }
+      }
+    }
+
+    /**
+     * \brief runs the main process
+     */
+    function run()
+    {
+
+      // process the file name first
+      $this->splitUpFilename();
+
+      // then see if there is any rule to process
+      $this->checkRules();
+    }
+  }
 
 
 // main program
@@ -174,16 +174,15 @@ $documents = array_merge($outbox, $inbox);
 // separate tags by comma and check if "ok" is present
 // move to sort if ok tag is contained
 foreach ($documents as $document) {
-	$basename = basename($document);
+  $basename = basename($document);
     preg_match(ATTRIBUTE_PATTERN, $basename, $attributes);
     $tags = explode(',', $attributes['tags']);
     if (in_array('ok', $tags)) {
         rename($document, '/data/sort/' . $basename);
-	}
+  }
 }
 
 
-$ppyrd->output("calling the sorter ...");
 chdir("/data/sort");
 
 /**
@@ -196,12 +195,11 @@ $pdfs = glob("*.pdf");
  */
 foreach ($pdfs as $pdf){
     $pdf=new pdfSorter($pdf, $db);
-		$pdf->run();
+    $pdf->run();
 }
 
 /*******************************************************************************/
 
-$ppyrd->output("Thanks for watching....");
 
 $db->close();
 

e62976a4b544c8131753ab1b7ff2bf22af891e28
Unknown <kramer@consider-it.de>
2018-01-09 15:02:32 +0100

    startup sequence changed
    * moved variable to dedicated config file
    * renamed internal variable
      * to be consistent on both sides (host/container)
      * to meet bash coding style
    * moved variable into paperyard „namespace“ to avoid collision
    * moved root path logic from config to start
    * renamed script (coding style)

---
+++ docker-run.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+function load_configuration()
+{
+    configuration_file="config/paperyard"
+
+    while IFS='= ' read lhs rhs
+    do
+        if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
+            rhs="${rhs%%\#*}"    # Del in line right comments
+            rhs="${rhs%%*( )}"   # Del trailing spaces
+            rhs="${rhs%\"*}"     # Del opening string quotes
+            rhs="${rhs#\"*}"     # Del closing string quotes
+            declare $lhs="$rhs"
+        fi
+    done < <(envsubst < $configuration_file)
+}
+
+function load_defaults()
+{
+    : ${paperyard_port:=80}
+    : ${paperyard_development:=false}
+}
+
+function start_development()
+{
+    docker run --name ppyrd --rm \
+          -v "$(pwd)/paperyard:/var/www/html/" \
+          -v "${paperyard_root}/${paperyard_database}:/data/database" \
+          -v "${paperyard_root}/${paperyard_scan}:/data/scan" \
+          -v "${paperyard_root}/${paperyard_inbox}:/data/inbox" \
+          -v "${paperyard_root}/${paperyard_outbox}:/data/outbox" \
+          -v "${paperyard_root}/${paperyard_sort}:/data/sort" \
+          -p "${paperyard_port}:80" \
+          -e COMMIT_COUNT=$(git rev-list --count MASTER) \
+          -i -t ppyrd_image
+}
+
+function start_production()
+{
+    docker run --name ppyrd --rm \
+          -v "${paperyard_root}/${paperyard_database}:/data/database" \
+          -v "${paperyard_root}/${paperyard_scan}:/data/scan" \
+          -v "${paperyard_root}/${paperyard_inbox}:/data/inbox" \
+          -v "${paperyard_root}/${paperyard_outbox}:/data/outbox" \
+          -v "${paperyard_root}/${paperyard_sort}:/data/sort" \
+          -p "${paperyard_port}:80" \
+          -i -t ppyrd_image
+}
+
+function splash()
+{
+    echo "Paperyard"
+
+    if ${paperyard_development}; then
+        echo "Environment: development"
+    else
+        echo "Environment: production"
+    fi
+
+    echo "Port: ${paperyard_port}"
+}
+
+function main()
+{
+    load_configuration
+
+    load_defaults
+
+    splash
+
+    if ${paperyard_development}; then
+        start_development
+    else
+        start_production
+    fi
+}
+
+main

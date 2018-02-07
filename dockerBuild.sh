f17901768fbf86f6566b472e9a33323f2d841863
Unknown <kramer@consider-it.de>
2018-02-07 11:21:49 +0100

    fixes #82
    - updated to ubuntu 17.10
    - updated to php 7.1 (because of ubuntu 17.10)
    - add no cache (--nc) option to docker build script
    
    Running docker-build is mandatory for this commit. Using the no cache option is recommended and may be necessary.

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,1 +1,8 @@
-docker build -t ppyrd_image . 
+#!/bin/bash
+
+if [ "$1" == "--nc" ]; then
+    echo "building without cache"
+    docker build --no-cache -t ppyrd_image .
+else
+    docker build -t ppyrd_image .
+fi
